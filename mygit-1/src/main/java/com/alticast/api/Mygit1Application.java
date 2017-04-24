32f514cec1e8177ee1496d7d17910906ef2d0ead
tisong <songtaeil@gmail.com>
2017-04-24 17:03:10 +0900

    where do you go?

---
+++ mygit-1/src/main/java/com/alticast/api/Mygit1Application.java
@@ -0,0 +1,12 @@
+package com.alticast.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mygit1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mygit1Application.class, args);
+	}
+}
