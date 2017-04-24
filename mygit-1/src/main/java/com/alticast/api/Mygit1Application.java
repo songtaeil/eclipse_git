87f9b2d8cb5005f09d4fc8e8f48ac15e22703701
tisong <songtaeil@gmail.com>
2017-04-24 17:12:12 +0900

    git-2 new myCar String

--- mygit-1/src/main/java/com/alticast/api/Mygit1Application.java
+++ mygit-1/src/main/java/com/alticast/api/Mygit1Application.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Mygit1Application {
 
+    private String myCar;
+    
 	public static void main(String[] args) {
 		SpringApplication.run(Mygit1Application.class, args);
 	}
